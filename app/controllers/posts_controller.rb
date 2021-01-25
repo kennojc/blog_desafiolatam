3dd70b477dcb1d31a294e4cb21ea8a01371438dc
Kenneth Johnson C <kennojc@gmail.com>
2021-01-24 21:14:56 -0300

    Added success or fail page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,10 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(title: params[:title], image_url: params[:image_url], content: params[:content]) 
+    
+    unless @post.save 
+      redirect_to root_path
+    end
   end
 end
